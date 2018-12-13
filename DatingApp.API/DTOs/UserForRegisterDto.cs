5a6db81fff0ff726828c9c6d945b26527bfaaaa0
Maciej_Mroz <mroz.m@eplan.pl>
2018-12-13 15:13:58 +0100

    added validation on userDTO and AuthController

--- DatingApp.API/DTOs/UserForRegisterDto.cs
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password long between 4 and 8")]
         public string Password { get; set; }
     }
 }
