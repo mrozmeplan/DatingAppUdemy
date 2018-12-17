d92b4e143d7688804eaa8ef16802a0e4a39aadd6
Maciej_Mroz <mroz.m@eplan.pl>
2018-12-17 08:40:33 +0100

    added output from home component to toggle the registration mode

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
 
   getValues() {
@@ -28,5 +28,9 @@ export class HomeComponent implements OnInit {
       console.log(error);
     });
 }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
 
 }
