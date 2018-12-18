5e6629aa1c3f70e17922f66384f771cf78272cf2
Maciej_Mroz <mroz.m@eplan.pl>
2018-12-18 09:20:22 +0100

    setted routing on login and logout

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private aletify: AlertifyService) { }
+  constructor(public authService: AuthService, private aletify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,8 @@ export class NavComponent implements OnInit {
       this.aletify.success('logged in succesfully');
     }, error => {
       this.aletify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -31,6 +34,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.aletify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
