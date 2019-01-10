27849d5219c73bc8cb4f2dbae0bf7dc5c0f807ce
Mroz <Mroz.M@eplan.pl>
2019-01-10 10:13:52 +0100

    implemented changing the main photo of the user in the backend

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -94,6 +94,36 @@ namespace DatingApp.API.Controllers
             }
             return BadRequest("Could not add the photo");
         }
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMainPhoto(int userId, int id)
+        {
+             if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+            return Unauthorized();
+            }
+            var user = await _repo.GetUser(userId);
 
+            if(!user.Photos.Any(p =>p.Id == id))
+            {
+                return Unauthorized();
+            }
+            var photoFormRepo = await _repo.GetPhoto(id);
+
+            if(photoFormRepo.IsMain)
+            {
+                return BadRequest("This is already main photo");
+            }
+
+            var currentMainPhoto = await _repo.GetMainPhotoForUser(userId);
+            currentMainPhoto.IsMain = false;
+
+            photoFormRepo.IsMain = true;
+
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+            return BadRequest("Could not set photo to main");
+        }
     }
 }
