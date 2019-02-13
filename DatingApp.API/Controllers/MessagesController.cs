8614ab5e2b836cf56a37418d0cdface54c8933bb
Mroz <Mroz.M@eplan.pl>
2019-02-13 09:07:02 +0100

    Fixed sending messages without picture of user issue

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -80,7 +80,9 @@ namespace DatingApp.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
         {
-            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            var sender = await _repo.GetUser(userId);
+
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
             {
                 return Unauthorized();
             }
@@ -96,10 +98,11 @@ namespace DatingApp.API.Controllers
 
             _repo.Add(message);
 
-            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+            
 
             if(await _repo.SaveAll())
             {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
                 return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
             }
 
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -120,17 +120,18 @@ namespace DatingApp.API.Data
             var messages = _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
                                             .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                                             .AsQueryable();
-            
-            switch (messageParams.MessageContainer) {
+
+            switch (messageParams.MessageContainer)
+            {
                 case "Inbox":
-                messages = messages.Where(u => u.RecipientId == messageParams.UserId);
-                break;
+                    messages = messages.Where(u => u.RecipientId == messageParams.UserId);
+                    break;
                 case "Outbox":
-                messages = messages.Where(u => u.SenderId == messageParams.UserId);
-                break;
+                    messages = messages.Where(u => u.SenderId == messageParams.UserId);
+                    break;
                 default:
-                messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false);
-                break;
+                    messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false);
+                    break;
             }
 
             messages = messages.OrderByDescending(d => d.MessageSent);
@@ -141,7 +142,7 @@ namespace DatingApp.API.Data
         {
             var messages = await _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
                                             .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                                            .Where(m => m.RecipientId == userId && m.SenderId == recipientId 
+                                            .Where(m => m.RecipientId == userId && m.SenderId == recipientId
                                             || m.RecipientId == recipientId && m.SenderId == userId)
                                             .OrderByDescending(m => m.MessageSent).ToListAsync();
 
